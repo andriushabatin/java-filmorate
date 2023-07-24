214e081f483037d6fb8fc87e22e2370dfaf58c13
andriushabatin <116507257+andriushabatin@users.noreply.github.com>
2023-07-24 18:06:10 +0300

    Revert "Controllers films users"

--- src/main/java/ru/yandex/practicum/filmorate/exception/film/InvalidDurationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception.film;
-
-public class InvalidDurationException extends Exception {
-    public InvalidDurationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/exception/user/InvalidBirthdayException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception.user;
-
-public class InvalidBirthdayException extends Exception {
-    public InvalidBirthdayException(String message) {
-        super(message);
-    }
-}
