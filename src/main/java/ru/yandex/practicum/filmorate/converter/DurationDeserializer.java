8973ee2fc732a7cf9bf78e5f37bd25d2a5f99bae
Андрей <andriushabatin@yandex.ru>
2023-07-24 16:59:03 +0300

    style: убрать лишние импорты в классе DurationDeserializer.

--- src/main/java/ru/yandex/practicum/filmorate/converter/DurationDeserializer.java
+++ src/main/java/ru/yandex/practicum/filmorate/converter/DurationDeserializer.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate.converter;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
