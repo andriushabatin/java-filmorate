544f6f088b848aaf4c45f34f217c2799c03f2979
Андрей <andriushabatin@yandex.ru>
2023-09-23 18:03:47 +0300

    test: добавить тест.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,15 +1,21 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.dao.film.impl.FilmDbStorage;
 import ru.yandex.practicum.filmorate.dao.user.impl.UserDbStorage;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
 
 
+import java.time.Duration;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,10 +28,13 @@ class FilmorateApplicationTests {
 	@Autowired
 	private final UserDbStorage userStorage;
 
+	@Autowired
+	private final FilmDbStorage filmStorage;
+
 	@Test
 	public void testCreateUser() {
 
-		User user = new User(1,
+		User user = new User(0,
 				"mail@mail.ru",
 				"dolore",
 				"Nick Name",
@@ -41,11 +50,30 @@ class FilmorateApplicationTests {
 	public void testFindUserById() {
 
 		Optional<User> userOptional = Optional.ofNullable(userStorage.findUserById(1));
-
 		assertThat(userOptional)
 				.isPresent()
 				.hasValueSatisfying(user ->
 						assertThat(user).hasFieldOrPropertyWithValue("id", 1)
 				);
+	}
+
+	@Test
+	public void testCreateFilm() {
+
+		Mpa mpa = new Mpa();
+		mpa.setId(1);
+
+		Film film = new Film(1,
+				"name",
+				"desc",
+				new Date(),
+				Duration.ofMinutes(100),
+				0,
+				mpa,
+				new HashSet<>()
+		);
+
+		Optional<Film> filmOptional = Optional.ofNullable(filmStorage.create(film));
+		assertThat(filmOptional).isPresent();
 	}
 }
