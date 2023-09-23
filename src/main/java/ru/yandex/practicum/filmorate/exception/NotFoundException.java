37ad238e66760f4b4ac4ac182ac3a4cc48983fbe
Андрей <andriushabatin@yandex.ru>
2023-09-23 18:34:41 +0300

    style.

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException() {}
+    public NotFoundException() {
+    }
+
     public NotFoundException(String message) {
         super(message);
     }
