b6994b52480456510b895e8168ca1d940fc802ef
Андрей <andriushabatin@yandex.ru>
2023-09-23 18:25:07 +0300

    style.

--- src/main/java/ru/yandex/practicum/filmorate/dao/film_genre/FilmGenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/film_genre/FilmGenreStorage.java
@@ -7,7 +7,10 @@ import java.util.List;
 public interface FilmGenreStorage {
 
     public void createFilmGenreRelations(int filmId, List<Genre> genres);
-    public List<Genre> findGenresByFilmId (int id);
+
+    public List<Genre> findGenresByFilmId(int id);
+
     public void updateFilmGenreRelations(int filmId, List<Genre> newGenres);
+
     public void deleteAllFilmGenreRelationsById(int filmId);
 }
