5010c5df8685245eedf171690be6b8faf514ee12
Андрей <andriushabatin@yandex.ru>
2023-10-05 12:42:48 +0300

    feat: Добавить контроллер и сервис для сущности Director.

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DirectorController {
+}
+
+
+
