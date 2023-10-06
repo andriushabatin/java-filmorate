78539faff11f255fcd604284d56c561359be42cf
Андрей <andriushabatin@yandex.ru>
2023-10-06 16:39:45 +0300

    feat: Реализовать метод createFilmDirectorRelations.

--- src/main/java/ru/yandex/practicum/filmorate/dao/film_director/impl/FilmDirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/film_director/impl/FilmDirectorDbStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.dao.film_director.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dao.film_director.FilmDirectorStorage;
 import ru.yandex.practicum.filmorate.model.Director;
@@ -11,8 +12,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FilmDirectorDbStorage implements FilmDirectorStorage {
 
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public void createFilmDirectorRelations(int filmId, List<Director> directors) {
 
+        String sqlQuery = "INSERT INTO film_director(film_id, director_id) " +
+                "values (?, ?)";
+
+        for (Director director : directors) {
+            jdbcTemplate.update(sqlQuery,
+                    filmId,
+                    director.getId());
+        }
     }
 }
