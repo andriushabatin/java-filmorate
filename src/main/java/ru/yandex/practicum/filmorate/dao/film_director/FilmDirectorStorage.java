f8fe6074b594ce8a612408bfd9d52146ffe1266f
Андрей <andriushabatin@yandex.ru>
2023-10-09 16:28:06 +0300

    style.

--- src/main/java/ru/yandex/practicum/filmorate/dao/film_director/FilmDirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/film_director/FilmDirectorStorage.java
@@ -7,8 +7,12 @@ import java.util.List;
 public interface FilmDirectorStorage {
 
     public void createFilmDirectorRelations(int filmId, List<Director> directors);
+
     public List<Director> findDirectorByFilmId(int id);
+
     public void updateFilmDirectorRelations(int id, List<Director> newDirector);
+
     public void deleteAllFilmDirectorRelationsById(int filmId);
+
     List<Integer> findFilmIdsOfDirector(int id, String sortBy);
 }
